86ca2e4676c9c37b09cecc6601fc088ccde96b24
Kasia Pohl <ksoloduc@gmail.com>
2020-03-02 22:46:42 +0100

    change env vars

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -6,39 +6,39 @@ import SEO from '../components/SEO';
 import contactStyles from './contact.module.scss';
 import ReCAPTCHA from 'react-google-recaptcha';
 
-const RECAPTCHA_KEY = '6Ldh5N0UAAAAAAdwyoX1v49CrbRSjzb3M464AYo6';
+const RECAPTCHA_KEY = '6LeBIN4UAAAAAE9wBGIeLgnh-Kaqzntx0tcTWOw5';
 const encode = (data) => {
 	return Object.keys(data).map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])).join('&');
 };
 const ContactForm = () => {
 	const { register, handleSubmit, errors, setValue, setError } = useForm();
 	const [ feedbackMsg, setFeedbackMsg ] = useState(null);
-	// const [ captcha, setCaptcha ] = useState(null);
+	const [ captcha, setCaptcha ] = useState(null);
 	let captchaRef = useRef(null);
 
-	// useEffect(
-	// 	() => {
-	// 		register({ required: 'Required', name: 'g-recaptcha-response' });
-	// 	},
-	// 	[ register ]
-	// );
+	useEffect(
+		() => {
+			register({ required: 'Required', name: 'g-recaptcha-response' });
+		},
+		[ register ]
+	);
 	const onSubmit = (data, e) => {
 		e.preventDefault();
-		// const captchaValue = captchaRef.current.getValue();
+		const captchaValue = captchaRef.current.getValue();
 		// console.log('On SUBMIT captchaVal (works!)' + captchaValue);
 		console.log(JSON.stringify(data));
-		// if (!captchaValue) {
-		// 	console.log('CAPTCHA missing!');
-		// 	setFeedbackMsg('Captcha is required');
-		// 	return;
-		// }
-		// // fetch('/?no-cache=1', {
+		if (!captchaValue) {
+			console.log('CAPTCHA missing!');
+			setFeedbackMsg('Captcha is required');
+			return;
+		}
+		// fetch('/?no-cache=1', {
 		fetch('/', {
 			method: 'POST',
 			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
 			body: encode({
 				'form-name': 'contact',
-				// 'g-recaptcha-response': captchaValue,
+				'g-recaptcha-response': captchaValue,
 				...data
 			})
 		})
@@ -58,7 +58,7 @@ const ContactForm = () => {
 			className={contactStyles.form}
 			name="contact"
 			method="post"
-			// data-netlify-recaptcha="true"
+			data-netlify-recaptcha="true"
 			netlify
 			netlify-honeypot="bot-field"
 			data-netlify="true"
@@ -112,7 +112,7 @@ const ContactForm = () => {
 			</div>
 			{errors.text && <span className={contactStyles.errorMessage}>please enter a message</span>}
 			<div className={contactStyles.submitContainer}>
-				{/* <ReCAPTCHA
+				<ReCAPTCHA
 					name="g-recaptcha-response"
 					ref={captchaRef}
 					sitekey="6Ldh5N0UAAAAAAdwyoX1v49CrbRSjzb3M464AYo6"
@@ -121,7 +121,7 @@ const ContactForm = () => {
 						setValue('g-recaptcha-response', val, true);
 						// console.log('end');
 					}}
-				/> */}
+				/>
 				{feedbackMsg && <h3>{feedbackMsg}</h3>}
 				<button className={contactStyles.linkButton} type="submit">
 					Send message
