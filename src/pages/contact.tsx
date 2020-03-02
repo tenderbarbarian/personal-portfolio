1c34fa8d9611b702ff841b522bfca3dc5a2217e6
Kasia Pohl <ksoloduc@gmail.com>
2020-03-02 15:57:19 +0100

    No captcha but FORM SUBMITS OK

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -4,11 +4,9 @@ import { useForm } from 'react-hook-form';
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
 import contactStyles from './contact.module.scss';
-
-///////////////////****************** */
 import ReCAPTCHA from 'react-google-recaptcha';
 
-// const RECAPTCHA_KEY = process.env.SITE_RECAPTCHA_KEY || '6Ldh5N0UAAAAAAdwyoX1v49CrbRSjzb3M464AYo6';
+const RECAPTCHA_KEY = process.env.SITE_RECAPTCHA_KEY || '6Ldh5N0UAAAAAAdwyoX1v49CrbRSjzb3M464AYo6';
 const encode = (data) => {
 	return Object.keys(data).map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])).join('&');
 };
@@ -111,7 +109,8 @@ const ContactForm = () => {
 				/>
 			</div>
 			{errors.text && <span className={contactStyles.errorMessage}>please enter a message</span>}
-			{/* <ReCAPTCHA
+			<div className={contactStyles.submitContainer}>
+				{/* <ReCAPTCHA
 				name="g-recaptcha-response"
 				ref={captchaRef}
 				sitekey={RECAPTCHA_KEY}
@@ -121,8 +120,7 @@ const ContactForm = () => {
 					// console.log('end');
 				}}
 			/> */}
-			{feedbackMsg && <h3>{feedbackMsg}</h3>}
-			<div className={contactStyles.submitContainer}>
+				{feedbackMsg && <h3>{feedbackMsg}</h3>}
 				<button className={contactStyles.linkButton} type="submit">
 					Send message
 				</button>
@@ -133,139 +131,6 @@ const ContactForm = () => {
 		</form>
 	);
 };
-
-////////////////////////////************** */
-
-// import ReCAPTCHA from 'react-google-recaptcha';
-// import { string } from 'prop-types';
-
-// const RECAPTCHA_KEY = process.env.SITE_RECAPTCHA_KEY || '6Ldh5N0UAAAAAAdwyoX1v49CrbRSjzb3M464AYo6';
-// const encode = (data) => {
-// 	return Object.keys(data).map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])).join('&');
-// };
-// // type FormData = {
-// // 	name: string;
-// // 	email: string;
-// // 	text: string;
-// // 	g-recaptcha-response: string;
-// // };
-
-// const ContactForm = ({ email }) => {
-// 	const { register, handleSubmit, errors, setValue, setError } = useForm();
-// 	const [ feedbackMsg, setFeedbackMsg ] = useState(null);
-// 	const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-// 	let captchaRef = useRef(null);
-
-// 	useEffect(
-// 		() => {
-// 			register({ required: 'Required', name: 'g-recaptcha-response' });
-// 		},
-// 		[ register ]
-// 	);
-// 	const onSubmit = (data, e) => {
-// 		e.preventDefault();
-// 		const captchaValue = captchaRef.current.getValue();
-// 		// console.log('On SUBMIT captchaVal (works!)' + captchaValue);
-// 		// console.log(JSON.stringify(data));
-// 		if (!captchaValue) {
-// 			// console.log('CAPTCHA missing!');
-// 			setFeedbackMsg('Captcha is required');
-// 			return;
-// 		}
-// 		fetch('/', {
-// 			method: 'POST',
-// 			headers: { 'Content-Type': 'application/x-www-form-urlencoded', 'Cache-Control': 'no-cache=1' },
-// 			body: encode({
-// 				'form-name': 'contact',
-// 				'g-recaptcha-response': captchaValue,
-// 				...data
-// 			})
-// 		})
-// 			.then((response) => {
-// 				console.log('POST RESPONSE:');
-// 				console.log({ response });
-// 				e.target.reset();
-// 				setFeedbackMsg(`Thanks for reaching out! I'll get back to you soon.`);
-// 			})
-// 			.catch((error) => {
-// 				setFeedbackMsg('Oops, something went wrong. The form could not be submitted.');
-// 				console.log(error);
-// 			});
-// 	};
-
-// 	return (
-// 		<form
-// 			onSubmit={handleSubmit(onSubmit)}
-// 			className={contactStyles.form}
-// 			name="contact"
-// 			method="post"
-// 			netlify
-// 			netlify-honeypot="bot-field"
-// 			data-netlify="true"
-// 			data-netlify-honeypot="bot-field"
-// 			data-netlify-recaptcha="true"
-// 		>
-// 			<input type="hidden" name="bot-field" />
-// 			<input type="hidden" name="form-name" value="contact" />
-// 			<div className={contactStyles.formEntry}>
-// 				<label>Name</label>
-// 				<input
-// 					type="text"
-// 					name="name"
-// 					placeholder="name"
-// 					ref={register({ required: true, maxLength: 30 })}
-// 					className={contactStyles.formInput}
-// 				/>
-// 			</div>
-// 			{errors.name && <span className={contactStyles.errorMessage}>Name is required</span>}
-// 			<div className={contactStyles.formEntry}>
-// 				<label>Email</label>
-// 				<input
-// 					type="email"
-// 					name="email"
-// 					ref={register({
-// 						required: true,
-// 						pattern
-// 					})}
-// 					placeholder="your@email.address"
-// 					className={contactStyles.formInput}
-// 				/>
-// 			</div>
-// 			{errors.email && <span className={contactStyles.errorMessage}>Please enter a valid email</span>}
-// 			<div className={contactStyles.formEntry}>
-// 				<label>Message</label>
-// 				<textarea
-// 					name="text"
-// 					rows="6"
-// 					ref={register({ required: true })}
-// 					placeholder="what's on your mind?"
-// 					className={contactStyles.formInput}
-// 				/>
-// 			</div>
-// 			{errors.text && <span className={contactStyles.errorMessage}>Please enter your message</span>}
-// 			<div className={contactStyles.submitContainer}>
-// 				<ReCAPTCHA
-// 					name="g-recaptcha-response"
-// 					ref={captchaRef}
-// 					sitekey={RECAPTCHA_KEY}
-// 					onChange={(val) => {
-// 						// console.log('ReCAPTCHA onChange: ', val);
-// 						setValue('g-recaptcha-response', val, true);
-// 						// console.log('end');
-// 					}}
-// 					data-netlify-recaptcha="true"
-// 				/>
-// 				{feedbackMsg && <h3>{feedbackMsg}</h3>}
-// 				<button className={contactStyles.linkButton}>Send message</button>
-// 				<small>
-// 					<a href={`mailto:${email}`}>or email me at {email}</a>
-// 				</small>
-// 			</div>
-// 		</form>
-// 	);
-// };
-
 const Contact = () => {
 	const data = useStaticQuery(graphql`
 		query {
