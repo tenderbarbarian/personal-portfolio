467f16019bcc7ee0c59a7136ee75a2c46f66a4b0
Kasia Pohl <ksoloduc@gmail.com>
2020-03-06 10:51:22 +0100

    download CV link fix

--- src/pages/cv.tsx
+++ src/pages/cv.tsx
@@ -6,33 +6,30 @@ import { Link } from 'gatsby';
 import cvStyles from './cv.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import cvPDF from '../images/KatarzynaPohl-CV-XL.pdf';
 
 const CV = () => (
-  <Layout>
-    <SEO title="CV" />
+	<Layout>
+		<SEO title="CV" />
 
-    <div className={cvStyles.downloadSection}>
-      <h2>Cirriculum vitae</h2>
-      <a
-        href="/documents/KatarzynaPohl-CV.pdf"
-        download=""
-        className={cvStyles.linkButton}
-      >
-        Download PDF <FontAwesomeIcon icon={faDownload} />
-      </a>
-    </div>
+		<div className={cvStyles.downloadSection}>
+			<h2>Cirriculum vitae</h2>
+			<a href={cvPDF} download="" className={cvStyles.linkButton}>
+				Download PDF <FontAwesomeIcon icon={faDownload} />
+			</a>
+		</div>
 
-    <div className={cvStyles.cvHolder}>
-      <Image />
-    </div>
+		<div className={cvStyles.cvHolder}>
+			<Image />
+		</div>
 
-    <div className={cvStyles.callToAction}>
-      <h3>Questions?</h3>
-      <Link to="/contact/" className={cvStyles.linkButton}>
-        Contact me
-      </Link>
-    </div>
-  </Layout>
+		<div className={cvStyles.callToAction}>
+			<h3>Questions?</h3>
+			<Link to="/contact/" className={cvStyles.linkButton}>
+				Contact me
+			</Link>
+		</div>
+	</Layout>
 );
 
 export default CV;
